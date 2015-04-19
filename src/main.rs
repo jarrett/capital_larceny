f75cc333b632f9a76f040f1fc842db7989344ac3
Jarrett Colby <jarrettcolby@gmail.com>
2015-04-19 15:08:00 -0400

    Switched to GLFW

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+extern crate glfw;
+extern crate gl;
+
+use glfw::{Context, Action, Key};
+use gl::types::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Initing GLFW");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).ok().expect("Failed to init glfw.");
+    
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 1));
+    glfw.window_hint(glfw::WindowHint::OpenglForwardCompat(true));
+    glfw.window_hint(glfw::WindowHint::OpenglProfile(glfw::OpenGlProfileHint::Core));
+    
+    println!("Creating window.");
+    let (mut window, events) = glfw.create_window(
+        1280, 960, "Capital Larceny", glfw::WindowMode::Windowed
+    ).expect("Failed to create GLFW window.");
+    
+    window.set_key_polling(true);
+    window.make_current();
+    
+    // Load the external functions. From the gl-rs crate.
+    gl::load_with(|s| window.get_proc_address(s));
+    
+    unsafe {
+        // Basic OpenGL configs.
+        gl::Enable(gl::DEPTH_TEST);
+        gl::Enable(gl::BLEND);
+        gl::DepthFunc(gl::LEQUAL);
+        gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+        gl::ClearColor(0.0, 0.0, 0.0, 0.0);
+    }
+    
+    println!("Starting main loop");
+    while !window.should_close() {
+        let (width, height) = window.get_size();
+        
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        }
+        
+        window.swap_buffers();
+        
+        glfw.poll_events();
+        glfw::flush_messages(&events);
+    }
 }
